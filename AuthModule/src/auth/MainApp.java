9b61fb4138eea59b93747cdd684891132a78737d
Juan Manuel Otálora Hernández <119978983+Otalorah@users.noreply.github.com>
2025-06-06 11:19:46 -0500

    Add AuthModule

---
+++ AuthModule/src/auth/MainApp.java
@@ -0,0 +1,14 @@
+package auth;
+
+public class MainApp {
+	
+    public static void main(String[] args) {
+    	// testToDoApp();
+    	// testBalotoApp();
+        Controller controller = new Controller();
+        controller.start();
+    }
+    
+
+  
+}
