c6a1337bf6bd895a675a8f151a83a153b72c1ed0
Juan Manuel Otálora Hernández <119978983+Otalorah@users.noreply.github.com>
2025-06-09 11:10:41 -0500

    Taller funcionando

---
+++ Taller funcionando/src/auth/MainApp.java
@@ -0,0 +1,10 @@
+package auth;
+
+public class MainApp {
+	
+    public static void main(String[] args) {
+        Controller controller = new Controller();
+        controller.start();
+    }
+      
+}
