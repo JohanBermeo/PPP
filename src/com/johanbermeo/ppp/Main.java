ef38c0fdf0303f4686a0b55f7e4806dc17d6f5d6
JohanBermeo <johan.bermeo29@outlook.com>
2025-06-10 13:56:56 -0500

    Reorganización del proyecto

---
+++ src/com/johanbermeo/ppp/Main.java
@@ -0,0 +1,8 @@
+import controller.BaseController;
+
+public class Main {
+    public static void main(String[] args) {
+        BaseController controller = new BaseController();
+        controller.start();
+    }  
+}
